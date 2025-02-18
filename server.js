707cdf768c281c379bb1b92334cdb68ad77b75c9
DavideCicoria <cicoriadavide@itis-molinari.eu>
2025-02-18 09:30:45 +0100

    fix bootstrap

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const path = require('path');
 const server = http.createServer(app);
 
 app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist")));
 
 app.post('/booking/add', async (req, res) => {
     try {
